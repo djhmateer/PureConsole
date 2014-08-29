32bd014c0f3a84fc875e6f821ee4fb63c562b0a2
Dave Mateer home <davemateer@gmail.com>
2014-08-29 07:35:22 +0100

    Refactored where the text file is to make simpler

--- PureConsole/Program.cs
+++ PureConsole/Program.cs
@@ -6,26 +6,26 @@ namespace PureConsole {
         static void Main(string[] args) {
             // Read a text file, logging to the screen
 
-            // Composition Root - the only place we new up stuff
+            // Composition Root - the 'only' place we new up stuff
             var logger = new Logger();
-            var reader = new TextFileReader(logger);
+            var textFileReader = new TextFileReader(logger);
 
             // Start the program
-            reader.ReadTextFile();
+            textFileReader.Read();
             Console.ReadLine();
         }
     }
 
-    // TextFileReader class has a dependency on Logger
+    // TextFileReader class has a dependency on ILogger
+    // Using ILogger (and not Logger) as then can inject in a 'fake' logger for testing
     public class TextFileReader {
         private readonly Logger logger;
         public TextFileReader(Logger logger) {
             this.logger = logger;
         }
 
-        public void ReadTextFile() {
-            logger.Log("Opening file");
-            var sr = new StreamReader(@"c:\temp\test.txt");
+        public void Read() {
+            var sr = new StreamReader(@"..\..\test.txt");
             var line = sr.ReadToEnd();
             logger.Log("File contents are: " + line);
         }
