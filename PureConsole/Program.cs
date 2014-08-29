884c5dbb056aca3d3b039e85e826be093d03b851
Dave Mateer home <davemateer@gmail.com>
2014-08-29 07:25:42 +0100

    first commit

---
+++ PureConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace PureConsole {
+    class Program {
+        static void Main(string[] args) {
+            // Read a text file, logging to the screen
+
+            // Composition Root - the only place we new up stuff
+            var logger = new Logger();
+            var reader = new TextFileReader(logger);
+
+            // Start the program
+            reader.ReadTextFile();
+            Console.ReadLine();
+        }
+    }
+
+    // TextFileReader class has a dependency on Logger
+    public class TextFileReader {
+        private readonly Logger logger;
+        public TextFileReader(Logger logger) {
+            this.logger = logger;
+        }
+
+        public void ReadTextFile() {
+            logger.Log("Opening file");
+            var sr = new StreamReader(@"c:\temp\test.txt");
+            var line = sr.ReadToEnd();
+            logger.Log("File contents are: " + line);
+        }
+    }
+
+    public class Logger {
+        public void Log(string message) {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ PureConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PureConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PureConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("141e5286-1163-4a5f-ba4c-5ce623463ba5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
